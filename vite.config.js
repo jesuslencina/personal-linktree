447f6fd48873023759925c4ba180b193204f108c
Jesus Lencina <jesusignaciolencina@gmail.com>
2024-01-30 13:37:08 -0300

    [WIP] Removed boilerplate and added libraries

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), qrcode()
   ],
   resolve: {
     alias: {
