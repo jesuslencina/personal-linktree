5e5826532644d9464150c809eb17b1a2bdb3957d
Jesus Lencina <jesusignaciolencina@gmail.com>
2024-02-19 11:43:22 -0300

    [PATCH] Applied security patch to redirect method

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -1,6 +1,6 @@
 export const redirect = (url, external = false) => {
     if (external) {
-        window.open(url)
+        window.open(url, "_blank", "noopener,noreferrer")
     } else {
         window.location.href = url
     }
